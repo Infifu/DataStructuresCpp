2bc59b4a32bb5ae9ca3829ce33eaef7dbfab5428
Infifu <iliiniliya2008@gmail.com>
2024-11-06 21:01:48 +0200

    Finished <3

--- DataStructuresCpp/main2.cpp
+++ DataStructuresCpp/main2.cpp
@@ -7,41 +7,40 @@ int main()
 	int choice = 0;
 	int element = 0;
 	Stack* s = new Stack;
-	while (choice != 6)
+	initStack(s);
+	while (choice != 5)
 	{
 		std::cout << "MENU: " << std::endl;
-		std::cout << "1.initStack " << std::endl;
-		std::cout << "2.Push " << std::endl;
-		std::cout << "3.Pop " << std::endl;
-		std::cout << "4.cleanStack " << std::endl;
-		std::cout << "5.isEmpty " << std::endl;
-		std::cout << "6.Leave " << std::endl;
+		std::cout << "1.Push " << std::endl;
+		std::cout << "2.Pop " << std::endl;
+		std::cout << "3.cleanStack " << std::endl;
+		std::cout << "4.isEmpty " << std::endl;
+		std::cout << "5.Leave " << std::endl;
 		std::cin >> choice;
 
 		switch (choice)
 		{
 		case 1:
-			initStack(s);
-			break;
-		case 2:
 			std::cout << "Enter number to push: ";
 			std::cin >> element;
 			push(s, element);
 			break;
+		case 2:
+			std::cout << "Popped: " << pop(s);
+			break;
 		case 3:
-			pop(s);
+			cleanStack(s);
+			initStack(s);
 			break;
 		case 4:
-			cleanStack(s);
-			break;
-		case 5:
 			std::cout << std::boolalpha << isEmpty(s);
 			break;
-		case 6:
+		case 5:
 			break;
 		default:
-
+			std::cout << "Choose a valid option";
 			break;
 		}
+		std::cout << "\n";
 	}
 }
